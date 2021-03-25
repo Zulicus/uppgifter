d78d2cbf5d601eb1da86742cdff277fe0625d9bb
Zulicus <70630780+Zulicus@users.noreply.github.com>
2021-03-25 10:54:40 +0100

    update

--- uppgift_2/src/main/java/com/example/demo/RPSController.java
+++ uppgift_2/src/main/java/com/example/demo/RPSController.java
@@ -31,11 +31,37 @@ public class RPSController {
 
 	// Code to show the current score
 	@RequestMapping(value = "/game/score", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public String singleplayer() {
+	public String score() {
 		// JSON-pattern for the score
 		String pattern = "{\"Player vs Player\":{\"Ties\":\"%s\",\"Player One\":{\"Wins\":\"%s\"},\"Player Two\":{\"Wins\":\"%s\"}},\"Single Player\":{\"Ties\":\"%s\",\"Player\":{\"Wins\":\"%s\"},\"Computer\":{\"Wins\":\"%s\"}}}";
 		return String.format(pattern, Score.MULTIPLAYERTIES, Score.PLAYE1RWINS, Score.PLAYER2WINS,
 				Score.SINGLEPLAYERTIES, Score.PLAYERWINS, Score.COMPUTERWINS);
 
 	}
+	
+	// Code to show the current score
+		@RequestMapping(value = "/game/score/set", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+		public String setScore(int multities,int p1wins,int p2wins,int singleties,int playerwins,int compwins) {
+			Score.MULTIPLAYERTIES=multities;
+			Score.PLAYE1RWINS=p1wins;
+			Score.PLAYER2WINS=p2wins;
+			Score.SINGLEPLAYERTIES=singleties;
+			Score.PLAYERWINS=playerwins;
+			Score.COMPUTERWINS=compwins;
+			return score();
+
+		}
+		
+		// Code to show the current score
+		@RequestMapping(value = "/game/score/reset", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+		public String resetScore() {
+			Score.MULTIPLAYERTIES=0;
+			Score.PLAYE1RWINS=0;
+			Score.PLAYER2WINS=0;
+			Score.SINGLEPLAYERTIES=0;
+			Score.PLAYERWINS=0;
+			Score.COMPUTERWINS=0;
+		return score();
+
+		}
 }
